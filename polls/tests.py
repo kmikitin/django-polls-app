ad8ad14e58f69b23ae6fc2edef667b765baa80e5
Kait <kait.mikitin@gmail.com>
2018-04-20 15:54:42 -0500

    added more automated tests

--- polls/tests.py
+++ polls/tests.py
@@ -9,13 +9,16 @@ from .models import Question
 # Create your tests here.
 def create_question(question_text, days):
     # Create a question with the given 'question_text' and publish the given number of 'days' offset to now 
-    # (negative for questions published in the past, postivie for questions that have yet to be published).
+    # (negative for questions published in the past, positive for questions that have yet to be published).
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
 
+def create_choice(question, choice_text):
+    return Choice.objects.create(question=question, choice_text=choice_text)
+
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
-        # If no questions exist, an apporpriate message is displayed
+        # If no questions exist, an appropriate message is displayed
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No polls are available")
@@ -58,15 +61,29 @@ class QuestionDetailViewTest(TestCase):
 
     def test_past_question(self):
         # The detail view of a question with a pub_date in the past displays the question's text.
-        past_question = create_question(question_text='Past Question.', days=-5)
+        past_question = create_question(question_text='Past question.', days=-5)
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
 
-# add testing for Results
+ 
+class QuestionResultViewTest(TestCase):
+    def test_future_question(self):
+        # The result view of a question wiht a pub_date in the future returns a 404 not found.
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:results', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        # The result view of a question with a pub_date in the past displays the voting results of that question.
+        past_question = create_question(question_text='Past question.', days=-5)
+        url = reverse('polls:results', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
 
 class QuestionModelTests(TestCase):
-
     def test_was_published_recently_with_future_question(self):
         # was_published_recently() returns False for questions whose pub_date is in the future
         time = timezone.now() + datetime.timedelta(days=30)
@@ -84,6 +101,19 @@ class QuestionModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+    # Still working on these tests:
+    # def test_question_has_three_choices(self):
+    #     # check that questions with choices are published
+    #     question = create_question('New question.', days=0)
+    #     first_choice = create_choice(question, 'Choice 1.')
+    #     second_choice = create_choice(question, 'Choice 2.')
+    #     third_choice = create_choice(question, 'Choice 3.')
+    #     self.assertContains(response, first_choice.choice_text)
+
+    # def test_question_does_not_have_choices(self):
+        # check that questions without choices are not published
+
 
 # add testing for Choices
 # add testing for Questions - Choices
